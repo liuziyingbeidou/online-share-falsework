69e3f846383c30ef2836805974cc3ab393c0e058
liuzy <liuziyingbeidou@163.com>
2019-01-23 23:40:24 +0800

    cros

--- src/main/java/com/venux/FalseWorkApplication.java
+++ src/main/java/com/venux/FalseWorkApplication.java
@@ -1,9 +1,11 @@
 package com.venux;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class FalseWorkApplication {
 
 	public static void main(String[] args) {
