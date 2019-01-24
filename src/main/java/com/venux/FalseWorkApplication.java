da26e14a8d95090f6473ba5cf0a748943c2a5bdf
liuzy <liuziyingbeidou@163.com>
2019-01-24 10:07:12 +0800

    cros

--- src/main/java/com/venux/FalseWorkApplication.java
+++ src/main/java/com/venux/FalseWorkApplication.java
@@ -1,8 +1,11 @@
 package com.venux;
 
+import com.venux.conf.SecurityFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -10,5 +13,14 @@ public class FalseWorkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FalseWorkApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean securityFilter() {
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		registrationBean.setFilter(new SecurityFilter());
+		registrationBean.addUrlPatterns("/*");
+		registrationBean.setOrder(Integer.MAX_VALUE);
+		return registrationBean;
 	}
 }
